a031132f7babcbce5837542c26f126a7b763a02d
milosbugarinovic <milos.bugarinovic@gmail.com>
2024-03-06 09:38:08 +0100

    chore: jest fix

--- test/jest.config.ts
+++ test/jest.config.ts
@@ -1,13 +1,19 @@
-export default {
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
 	clearMocks: true,
 	extensionsToTreatAsEsm: ['.ts'],
+	maxConcurrency: 1,
 	moduleFileExtensions: ['js', 'ts'],
+	moduleNameMapper: {
+		'^src/(.*)$': '<rootDir>/src/$1',
+	},
 	preset: 'ts-jest/presets/default-esm',
 	setupFilesAfterEnv: ['jest-extended/all'],
-
-	// preset: 'ts-jest',
 	testEnvironment: 'node',
-
+	testMatch: ['<rootDir>/src/**/*.test.ts'],
 	testPathIgnorePatterns: ['/node_modules/'],
 	transform: {},
 }
+
+export default jestConfig
